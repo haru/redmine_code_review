6801159e61a257f69da2cb80ede95fb06552c579
haru.ii.a9i1e <devnull@localhost>
2009-05-05 12:49:59 +0000

    fixes #18. GPLライセンス文追加。

--- init.rb
+++ init.rb
@@ -1,12 +1,28 @@
+# Code Review plugin for Redmine
+# Copyright (C) 2009  Haruyuki Iida
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require 'redmine'
 require 'code_review_application_hooks'
 
 
 Redmine::Plugin.register :redmine_code_review do
   name 'Redmine Code Review plugin'
-  author 'Haru Iida'
+  author 'Haruyuki Iida'
   description 'This is a Code Review plugin for Redmine'
-  version '0.0.1'
+  version '0.1.0'
   requires_redmine :version_or_higher => '0.8.0'
 
   project_module :code_review do
