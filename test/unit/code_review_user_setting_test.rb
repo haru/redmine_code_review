804c19199f22b4cf3743b747e0e441bcefaca77a
haru.ii.a9i1e <devnull@localhost>
2009-06-28 06:29:08 +0000

    refs #108. 取りあえずエラーにならないようにした。

---
+++ test/unit/code_review_user_setting_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class CodeReviewUserSettingTest < Test::Unit::TestCase
+  fixtures :code_review_user_settings
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
