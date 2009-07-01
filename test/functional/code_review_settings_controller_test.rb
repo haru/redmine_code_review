5b9cbdfc62e38337b77f5c5bbfc34878c65876fd
haru.ii.a9i1e <devnull@localhost>
2009-07-01 15:55:27 +0000

    refs #108. テスト追加中

--- test/functional/code_review_settings_controller_test.rb
+++ test/functional/code_review_settings_controller_test.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class CodeReveiewControllerTest < ActionController::TestCase
+class CodeReviewSettingsControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   def test_truth
     assert true
