7e72b3b425703b13768a9653b1bf378f889194e6
haru.ii.a9i1e <devnull@localhost>
2009-07-06 15:20:55 +0000

    refs #108. テスト追加中

--- test/functional/code_review_settings_controller_test.rb
+++ test/functional/code_review_settings_controller_test.rb
@@ -1,8 +1,44 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class CodeReviewSettingsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def setup
+    @controller = CodeReviewSettingsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    @request.env["HTTP_REFERER"] = '/'
+    enabled_module = EnabledModule.new
+    enabled_module.project_id = 1
+    enabled_module.name = 'code_review'
+    enabled_module.save
+
+    User.current = nil
+    roles = Role.find(:all)
+    roles.each {|role|
+      role.permissions << :view_code_review
+      role.save
+    }
+  end
+
+  def test_show
+    @request.session[:user_id] = 1
+    get :show, :id => 1
+    assert_response :success
+
+    @request.session[:user_id] = User.anonymous.id
+    get :show, :id => 1
+    assert_response 403
+  end
+
+  def test_update
+    @request.session[:user_id] = User.anonymous.id
+    get :update, :id => 1
+    assert_response 403
+
+    @request.session[:user_id] = 1
+    setting = CodeReviewUserSetting.find_or_create(1)
+    get :update, :id => 1, :setting => {:mail_notification => setting.mail_notification, :user_id => setting.user_id, :id => setting.id}
+    assert_response :redirect
+    project = Project.find(1)
+    assert_redirected_to :action => 'show', :id => project
   end
 end
