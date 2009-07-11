263c9e21e82f6aee2920c992b8177d6e16241424
haru.ii.a9i1e <devnull@localhost>
2009-07-11 05:36:23 +0000

    バージョン変更

--- init.rb
+++ init.rb
@@ -24,7 +24,7 @@ Redmine::Plugin.register :redmine_code_review do
   author 'Haruyuki Iida'
   url "http://www.r-labs.org/projects/show/codereview" if respond_to?(:url)
   description 'This is a Code Review plugin for Redmine'
-  version '0.1.8.5'
+  version '0.1.8.6'
   requires_redmine :version_or_higher => '0.8.3'
 
   project_module :code_review do
