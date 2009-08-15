7e394a61e67054304c5d06fd797c66eb03668953
haru.ii.a9i1e <devnull@localhost>
2009-08-15 07:14:09 +0000

    test追加

---
+++ test/functional/issues_controller_test.rb
@@ -0,0 +1,72 @@
+# Redmine - project management software
+# Copyright (C) 2006-2008  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require File.dirname(__FILE__) + '/../test_helper'
+require 'issues_controller'
+
+# Re-raise errors caught by the controller.
+class IssuesController; def rescue_action(e) raise e end; end
+
+class IssuesControllerTest < Test::Unit::TestCase
+  fixtures :projects,
+           :users,
+           :roles,
+           :members,
+           :issues,
+           :issue_statuses,
+           :versions,
+           :trackers,
+           :projects_trackers,
+           :issue_categories,
+           :enabled_modules,
+           :enumerations,
+           :attachments,
+           :workflows,
+           :custom_fields,
+           :custom_values,
+           :custom_fields_trackers,
+           :time_entries,
+           :journals,
+           :journal_details,
+           :code_reviews
+  
+  def setup
+    @controller = IssuesController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+    User.current = nil
+    enabled_module = EnabledModule.new
+    enabled_module.project_id = 1
+    enabled_module.name = 'code_review'
+    enabled_module.save
+    enabled_module = EnabledModule.new
+    enabled_module.project_id = 2
+    enabled_module.name = 'code_review'
+    enabled_module.save
+    roles = Role.find(:all)
+    roles.each {|role|
+      role.permissions << :view_code_review
+      role.save
+    }
+  end
+
+  def test_show
+    @request.session[:user_id] = 1
+    get :show, :id => 1
+
+  end
+end
