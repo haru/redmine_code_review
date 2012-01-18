26dfa3d6095659d94b8ba35c9c8365c7b57d4e08
haru_iida <devnull@localhost>
2012-01-18 22:58:05 +0900

    Change version.

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # Code Review plugin for Redmine
-# Copyright (C) 2009-2011  Haruyuki Iida
+# Copyright (C) 2009-2012  Haruyuki Iida
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -58,8 +58,8 @@ Redmine::Plugin.register :redmine_code_review do
   author 'Haruyuki Iida'
   url "http://www.r-labs.org/projects/show/codereview" if respond_to?(:url)
   description 'This is a Code Review plugin for Redmine'
-  version '0.4.6'
-  requires_redmine :version_or_higher => '1.2.0'
+  version '0.4.7'
+  requires_redmine :version_or_higher => '1.3.0'
 
   project_module :code_review do
     permission :view_code_review, {:code_review => [:update_diff_view, :update_attachment_view, :update_revisions_view, :index, :show]}
