70a8c6484fe855dae31b6d4320efbff6ec635af4
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2014-04-20 11:34:17 +0900

    fixes simplecov instruction.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 # Code Review plugin for Redmine
-# Copyright (C) 2010-2012  Haruyuki Iida
+# Copyright (C) 2010-2014  Haruyuki Iida
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -14,6 +14,16 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+
+SimpleCov.start do
+  root File.expand_path(File.dirname(__FILE__) + '/..')
+  add_filter "/test/"
+end
 
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
