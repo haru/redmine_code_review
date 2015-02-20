cb18bad6591bd54c32bf965e53e97725441040bb
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2015-02-21 08:17:07 +0900

    set version to 0.7.0

--- init.rb
+++ init.rb
@@ -57,8 +57,8 @@ Redmine::Plugin.register :redmine_code_review do
   author_url 'http://twitter.com/haru_iida'
   url "http://www.r-labs.org/projects/show/codereview" if respond_to?(:url)
   description 'This is a Code Review plugin for Redmine'
-  version '0.6.5'
-  requires_redmine :version_or_higher => '2.1.0'
+  version '0.7.0'
+  requires_redmine :version_or_higher => '3.0.0'
 
   project_module :code_review do
     permission :view_code_review, {:code_review => [:update_diff_view, :update_attachment_view, :update_revisions_view, :index, :show]}
