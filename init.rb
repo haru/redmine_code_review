309414495841ca18ebb43caee5adf44b3a262078
Jens Kraemer <jk@jkraemer.net>
2018-03-08 11:46:35 +0800

    removes unnecessary require statements

--- init.rb
+++ init.rb
@@ -15,18 +15,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 require 'redmine'
-begin
-  require 'config/initializers/session_store.rb'
-rescue LoadError
-end
-require 'gravatar'
+
 require 'code_review_application_hooks'
-require 'code_review_change_patch'
-require 'code_review_changeset_patch'
-require 'code_review_issue_patch'
 require 'code_review_issue_hooks'
-require 'code_review_projects_helper_patch'
-require 'code_review_attachment_patch'
 
 Rails.configuration.to_prepare do
   unless Change.included_modules.include? CodeReviewChangePatch
