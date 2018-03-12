9f1491d330b466ea7a1a7d0c5b73b49deac06ac3
Jens Kraemer <jk@jkraemer.net>
2018-03-12 10:01:26 +0800

    refactor progress_for_changeset helper to use tag helpers
    - also removes the raw call which is now unnecessary and replaces it with java
      script escaping

--- app/helpers/code_review_helper.rb
+++ app/helpers/code_review_helper.rb
@@ -36,26 +36,36 @@ module CodeReviewHelper
   end
 
   def progress_for_changeset(changeset)
-    if changeset.review_count > 0
-      progress = '<span style="white-space: nowrap">' + progress_bar([changeset.closed_review_pourcent, changeset.completed_review_pourcent],
-        :width => '60px',
-        :legend => "#{changeset.closed_review_count}/#{changeset.review_count} #{l(:label_closed_issues)}") + '</span>'
-    elsif changeset.assignment_count > 0
-      if (changeset.open_assignment_count > 0)
-        progress = '<p class="progress-info" style="white-space: nowrap">' + l(:code_review_assigned) + '</p>'
-      else
-        progress = '<p class="progress-info" style="white-space: nowrap">' + l(:code_review_reviewed) + '</p>'
+    progress = if changeset.review_count > 0
+      content_tag(:span, style: "white-space: nowrap") do
+        progress_bar(
+          [changeset.closed_review_pourcent, changeset.completed_review_pourcent],
+          :width => '60px',
+          :legend => "#{changeset.closed_review_count}/#{changeset.review_count} #{l(:label_closed_issues)}"
+        )
       end
 
-    else
-      progress = '<p class="progress-info" style="white-space: nowrap;"><span  style="white-space: nowrap;">' +
-        l(:lable_no_code_reviews) +
-        '</span>:<span  style="white-space: nowrap;">' +
+    elsif changeset.assignment_count > 0
+      if (changeset.open_assignment_count > 0)
+        l(:code_review_assigned)
+      else
+        l(:code_review_reviewed)
+      end
+
+    elsif User.current.allowed_to?(:assign_code_review, @project)
+      content_tag(:span, style: "white-space: nowrap") do
+        l(:lable_no_code_reviews)
+      end + ':'.html_safe +
+      content_tag(:span, style: "white-space: nowrap") do
         link_to(l(:label_assign_review), {:controller => 'code_review',
           :action => 'assign', :id=>@project,
           :rev => changeset.revision,
-          :changeset_id => changeset.id}) +
-          '</span></p>' if User.current.allowed_to?(:assign_code_review, @project)
+          :changeset_id => changeset.id})
+      end
     end
+
+    content_tag(:p, class: "progress-info", style: "white-space: nowrap;") do
+      progress
+    end if progress
   end
 end
