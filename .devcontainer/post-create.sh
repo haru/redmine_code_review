37a18b0a9c6f70e9c130a6c204034ce2e078b87f
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2023-11-03 03:10:03 +0000

    set devcontainer to Redmie 5.1 Ruby, 3.2

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,6 +7,7 @@ then
     cp plugins/${PLUGIN_NAME}/Gemfile_for_test plugins/${PLUGIN_NAME}/Gemfile 
 fi
 cp plugins/${PLUGIN_NAME}/test/fixtures/*.yml test/fixtures
+ln -s /workspaces/${PLUGIN_NAME}/.devcontainer/launch.json .vscode/launch.json
 
 bundle install 
 bundle exec rake redmine:plugins:migrate
