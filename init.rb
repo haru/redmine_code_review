c50fe9de3ac4f62fb12613ff1af9263d05101853
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2025-07-26 03:46:33 +0000

    1.2.2

--- init.rb
+++ init.rb
@@ -36,7 +36,7 @@ Redmine::Plugin.register :redmine_code_review do
   author_url 'http://twitter.com/haru_iida'
   url "http://www.redmine.org/plugins/redmine_code_review" if respond_to?(:url)
   description 'This is a Code Review plugin for Redmine'
-  version '1.2.1'
+  version '1.2.2'
   requires_redmine :version_or_higher => '5.0.0'
 
   project_module :code_review do
