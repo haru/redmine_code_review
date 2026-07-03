005e8f068849336d5fc56e02e453e58ecb961bfb
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2026-07-03 14:56:44 +0000

    style: unify string delimiters and improve code formatting in init.rb

--- init.rb
+++ init.rb
@@ -14,45 +14,45 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-require 'redmine'
+require "redmine"
 begin
-  require 'config/initializers/session_store.rb'
+  require "config/initializers/session_store.rb"
 rescue LoadError
 end
 
 $LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
 
-require 'code_review_application_hooks'
-require 'code_review_change_patch'
-require 'code_review_changeset_patch'
-require 'code_review_issue_patch'
-require 'code_review_issue_hooks'
-require 'code_review_projects_helper_patch'
-require 'code_review_attachment_patch'
+require "code_review_application_hooks"
+require "code_review_change_patch"
+require "code_review_changeset_patch"
+require "code_review_issue_patch"
+require "code_review_issue_hooks"
+require "code_review_projects_helper_patch"
+require "code_review_attachment_patch"
 
 Redmine::Plugin.register :redmine_code_review do
-  name 'Redmine Code Review plugin'
-  author 'Haruyuki Iida'
-  author_url 'http://twitter.com/haru_iida'
+  name "Redmine Code Review plugin"
+  author "Haruyuki Iida"
+  author_url "http://twitter.com/haru_iida"
   url "http://www.redmine.org/plugins/redmine_code_review" if respond_to?(:url)
-  description 'This is a Code Review plugin for Redmine'
-  version '1.2.2'
-  requires_redmine :version_or_higher => '5.1.0'
+  description "This is a Code Review plugin for Redmine"
+  version "1.2.2"
+  requires_redmine :version_or_higher => "6.0.0"
 
   project_module :code_review do
-    permission :view_code_review, {:code_review => [:update_diff_view, :update_attachment_view, :update_revisions_view, :index, :show]}, {:read => true}
-    permission :add_code_review, {:code_review => [:new, :reply, :forward_to_revision, :preview]}, :require => :member
-    permission :edit_code_review, {:code_review => [:update]}, :require => :member
-    permission :delete_code_review, {:code_review => [:destroy]}, :require => :member
-    permission :assign_code_review, {:code_review => [:assign]}, :require => :member
-    permission :code_review_setting, {:code_review_settings => [:show, :update, :add_filter, :edit_filter, :sort]}, :require => :member
+    permission :view_code_review, { :code_review => [:update_diff_view, :update_attachment_view, :update_revisions_view, :index, :show] }, { :read => true }
+    permission :add_code_review, { :code_review => [:new, :reply, :forward_to_revision, :preview] }, :require => :member
+    permission :edit_code_review, { :code_review => [:update] }, :require => :member
+    permission :delete_code_review, { :code_review => [:destroy] }, :require => :member
+    permission :assign_code_review, { :code_review => [:assign] }, :require => :member
+    permission :code_review_setting, { :code_review_settings => [:show, :update, :add_filter, :edit_filter, :sort] }, :require => :member
   end
 
-  menu :project_menu, :code_review, {:controller => 'code_review', :action => 'index'}, :caption => :code_reviews,
-                                                                                        :if => Proc.new { |project|
-                                                                                          setting = CodeReviewProjectSetting.find_or_create(project)
-                                                                                          project.repository != nil and setting and !setting.hide_code_review_tab
-                                                                                        }, :after => :repository
+  menu :project_menu, :code_review, { :controller => "code_review", :action => "index" }, :caption => :code_reviews,
+                                                                                          :if => Proc.new { |project|
+                                                                                            setting = CodeReviewProjectSetting.find_or_create(project)
+                                                                                            project.repository != nil and setting and !setting.hide_code_review_tab
+                                                                                          }, :after => :repository
 
   Redmine::WikiFormatting::Macros.register do
     desc "This is my macro link to code review"
@@ -62,7 +62,7 @@ Redmine::Plugin.register :redmine_code_review do
       return nil if review_id == 0
       review = CodeReview.find(review_id)
       return nil unless review
-      link_to(l(:label_review) + '#' + review.id.to_s, :controller => 'code_review', :action => 'show', :id => review.project, :review_id => review.id)
+      link_to(l(:label_review) + "#" + review.id.to_s, :controller => "code_review", :action => "show", :id => review.project, :review_id => review.id)
     end
   end
 end
